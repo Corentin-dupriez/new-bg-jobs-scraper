399a455e7a658d9872e31e70345d2739dd6270b8
Corentin Dupriez <dupriez.c@icloud.com>
2025-11-13 11:31:01 +0200

    Fixed company_name in jobs spider. Removed example models

--- dbt_jobs_analytics/models/staging/staging_jobs.sql
+++ dbt_jobs_analytics/models/staging/staging_jobs.sql
@@ -1,5 +1,23 @@
+{% set columns = {
+  "company_name": "company_name",
+  'job_title': 'job_title',
+  'location': 'location',
+  'min_salary': "ifnull(split(split(salary, ' лв.')[1], ' - ')[1], 'Not communicated') as min_salary",
+  "max_salary": "ifnull(split(split(salary, ' лв.')[1], ' - ')[2], 'Not communicated') as max_salary",
+  "tech_stack": "tech_stack",
+  "listing_url": "listing_url",
+  "description": "description", 
+  "date_posted": "date_posted",
+  "date_scraped": "date_scraped"
+  } 
+%}
+
+
 WITH jobs_cte AS (
-  SELECT * 
+  SELECT 
+  {% for c, query in columns.items() %}
+    {{ query }} {% if not loop.last %},{% endif %}
+  {% endfor %}
   FROM {{ source('main','jobs')}}
 )
 
